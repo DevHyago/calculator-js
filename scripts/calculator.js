2e6dc054063835597b6389eb6ff812e8bf2e6ad3
Hyago Vieira <dev.hyago@outlook.com>
2022-02-07 06:19:08 -0300

    Creating javascript file structures and programming date and time screen

---
+++ scripts/calculator.js
@@ -0,0 +1,1 @@
+window.calculatorJs = new CalcController();
---
+++ scripts/controller/calcController.js
@@ -0,0 +1,65 @@
+class CalcController{
+
+   constructor(){
+
+      this._locale = 'pt-BR';
+      this._displayCalcEl = document.querySelector('#result');
+      this._dateEl = document.querySelector('#date');
+      this._timeEl = document.querySelector('#hour');
+      this._currentDate;
+      this.initialize();
+
+   }
+
+   initialize(){
+      this.setDisplayDateTime();
+      setInterval(() => {
+         this.setDisplayDateTime();
+      }, 1000);
+
+   }
+
+   setDisplayDateTime(){
+      this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+         day: "2-digit",
+         month: "long",
+         year: "numeric"
+      });
+      this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+   }
+
+   /* methods get's and set's */
+   get displayTime(){
+      return this._timeEl.innerHTML;
+   }
+
+   set displayTime(value){
+      this._timeEl.innerHTML = value;
+   }
+
+   get displayDate(){
+      return this._dateEl.innerHTML;
+   }
+
+   set displayDate(value){
+      this._dateEl.innerHTML = value;
+   }
+
+   get displayCalc(){
+      return this._displayCalcEl.innerHTML;
+   }
+
+   set displayCalc(value){
+      this._displayCalcEl.innerHTML = value;
+   }
+
+   get currentDate(){
+      return new Date();
+   }
+
+   set currentDate(value){
+      this._currentDate = value;
+   }
+
+
+}
